ba38ca9743246623637f11587733eee8f80f3b3d
NahyeonS2 <77606312+NahyeonS2@users.noreply.github.com>
2021-01-21 18:16:38 +0900

    단계2 1번

---
+++ 1330.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+X = int(input())
+if 90<=X<=100:
+    print('A')
+elif 80<=X<=89:
+    print('B')
+elif 70<=X<=79:
+    print('C')
+elif 60<=X<=69:
+    print('D')
+else:
+    print('F1')
