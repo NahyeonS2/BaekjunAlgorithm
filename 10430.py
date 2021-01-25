dcd4d6c1e9d6d5087ed9c00a28c7a301f277f90e
NahyeonS2 <77606312+NahyeonS2@users.noreply.github.com>
2021-01-25 13:50:30 +0900

    Update 10430.py

--- 10430.py
+++ 10430.py
@@ -1,20 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-a = int(input())
-b = int(input())
-A = a//100
-B = (a-A*100)//10
-C = a-A*100-B*10
-D = b//100
-E = (b-D*100)//10
-F = b-D*100-E*10
-
-print( a*F )
-print( a*E )
-print( a*D )
-print( a*F + a*E*10 + a*D*100 )
+a, b, c = input().split()
+A = int(a)
+B = int(b)
+C = int(c)
+print((A+B)%C)
+print( ((A%C) + (B%C))%C )
+print((A*B)%C)
+print(((A%C) * (B%C))%C)
