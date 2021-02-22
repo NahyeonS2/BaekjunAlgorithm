8bef17a22feefd0bcd78ada5d41bca1983ea9053
NahyeonS2 <77606312+NahyeonS2@users.noreply.github.com>
2021-02-22 16:29:05 +0900

    단계3 2번

---
+++ 10950.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 22 13:42:17 2021
+
+@author: IWTL
+"""
+
+T = int(input())
+for t in range(0, T):
+    a, b = map(int, input().split())
+    print(a+b)
